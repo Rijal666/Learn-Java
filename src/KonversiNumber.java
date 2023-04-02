aa94f75672695d4e089dd1b935eff85425058e81
Rijal666 <rizkirizalmualim2@gmail.com>
2023-04-02 19:32:21 +0700

    tipe data number

---
+++ src/KonversiNumber.java
@@ -0,0 +1,17 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+        // Widening Casting(Otomatis) : byte > short > int > long > float > double
+        // Narrowing Casting (Manual) : double > float > long > int > char > short > byte
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+        long iniLong = iniInt;
+        float iniFloat = iniLong;
+        double iniDouble = iniFloat;
+
+        float iniFloat2 = (float) iniDouble;
+        long iniLong2 = (long) iniFloat2;
+        int iniInt2 = (int) iniLong2;
+        short iniShort2 = (short) iniInt2;
+    }
+}
